fcb1e5dc9c84e4fa5849019fcf86dfba6941f99a
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-10-26 13:58:17 +0200

    wip dendrite bm

---
+++ src/rgc_dendrite_bm.h
@@ -0,0 +1,28 @@
+#ifndef RGC_DENDRITE_BM_
+#define RGC_DENDRITE_BM_
+
+#include "biodynamo.h"
+
+namespace bdm {
+using namespace std;
+
+
+// Define rgc dendrite behavior
+struct RGC_dendrite_BM : public BaseBiologyModule {
+  BDM_STATELESS_BM_HEADER(RGC_dendrite_BM, BaseBiologyModule, 1);
+
+public:
+  RGC_dendrite_BM() : BaseBiologyModule(gAllEventIds) {}
+
+  void Run(SimObject* so) override {
+    if (auto* ne = dynamic_cast<MyNeurite*>(so)) {
+      auto* sim = Simulation::GetActive();
+      auto* random = sim->GetRandom();
+
+    } // end if MyNeurite
+  } // end Run()
+}; // end biologyModule RGC_dendrite_BM
+
+}
+
+#endif
