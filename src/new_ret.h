007b3d2e7259bc684469697e92fefa170fcf12b1
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2020-01-08 16:06:30 +0100

    create all cells

--- src/new_ret.h
+++ src/new_ret.h
@@ -58,20 +58,13 @@ inline int Simulate(int argc, const char** argv) {
 
   // create cells
   // CellCreator(param->min_bound_, param->max_bound_, num_cells, -1);
-  /*
+
   CellCreator(param->min_bound_, param->max_bound_, 31, 0);
   CellCreator(param->min_bound_, param->max_bound_, 31, 1);
   CellCreator(param->min_bound_, param->max_bound_, 31, 2);
   CellCreator(param->min_bound_, param->max_bound_, 31, 3);
-  CellCreator(param->min_bound_, param->max_bound_, 62, 5);
-  */
-
-  // nb root: 4.02 - std: 1.14
-  // diameter: 142.36 - std: 30.08
-  // branchin nb: 117.3 - std: 75.98
-  // aniso: 0.46 - std: 0.22
-  /*
   CellCreator(param->min_bound_, param->max_bound_, 5, 4);
+  CellCreator(param->min_bound_, param->max_bound_, 62, 5);
   CellCreator(param->min_bound_, param->max_bound_, 5, 6);
   CellCreator(param->min_bound_, param->max_bound_, 5, 7);
   CellCreator(param->min_bound_, param->max_bound_, 15, 8);
@@ -98,7 +91,6 @@ inline int Simulate(int argc, const char** argv) {
   CellCreator(param->min_bound_, param->max_bound_, 5, 116);
   CellCreator(param->min_bound_, param->max_bound_, 5, 117);
   CellCreator(param->min_bound_, param->max_bound_, 5, 118);
-  */
 
   CellCreator(param->min_bound_, param->max_bound_, 10, 200); // sustained
   CellCreator(param->min_bound_, param->max_bound_, 10, 201); // transient
